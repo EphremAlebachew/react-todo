67f0cefba14ce3029a921b2d129302d24008c072
Ephrem Alebachew Tebeje <128262759+EphremAlebachew@users.noreply.github.com>
2024-04-07 21:34:34 -0500

    ass-2-1-with-vite

--- src/Todolist/TodoList.jsx
+++ src/Todolist/TodoList.jsx
@@ -9,7 +9,7 @@ const greetings = "Good morning";
  function TodoList() {
   return (
     <div>
-      <h1>{greetings}, Mr Frank</h1>
+      <h1>{greetings}, Frank</h1>
       <ul>
         {List.map(list => (
           <li key={list.id}>{list.title}</li>
