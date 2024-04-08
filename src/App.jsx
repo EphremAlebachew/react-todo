70040da044e4a071bc41a0432b19cd44a595c284
Ephrem Alebachew Tebeje <128262759+EphremAlebachew@users.noreply.github.com>
2024-04-08 01:16:45 -0500

    ass-3-2-with-vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import TodoList from "./Todolist/TodoList";
 import AddTodoForm from "./AddTodoForm/AddTodoForm";
+
 function App() {
-  const [todoList, setTodoList] = useState([]); 
+  const [todoList, setTodoList] = useState([]);
+
+  const handleAddTodo = (newTodoTitle) => {
+    const newTodo = { id: todoList.length + 1, title: newTodoTitle };
+    setTodoList([...todoList, newTodo]);
+  };
+
   return (
     <div>
+      
       <h1>Todo List</h1>
 
-      <AddTodoForm />
-      <TodoList  todoList={todoList}/>
+      <AddTodoForm onAddTodo={handleAddTodo} />
+      <TodoList todoList={todoList} />
     </div>
   );
 }
