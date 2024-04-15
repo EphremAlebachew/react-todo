efba77b4b040e3044ed7404fc87b0e64dd9dbfa9
Ephrem Alebachew Tebeje <128262759+EphremAlebachew@users.noreply.github.com>
2024-04-15 01:29:28 -0500

    ass-6-2-with-vite

--- src/InputWithLabel/InputWithLabel.jsx
+++ src/InputWithLabel/InputWithLabel.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const InputWithLabel = (props) => {
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
   return (
-    <div>
+    <>
       <label htmlFor="todoTitle">{props.label}:</label>
       <input
         id="todoTitle"
         type="text"
         value={props.todoTitle}
         onChange={props.handleTitleChange}
+        ref={inputRef} 
       />
-    </div>
+    </>
   );
 };
 
