d99776e96531883ce91d7265bf06bdded145fb2c
Ephrem Alebachew Tebeje <128262759+EphremAlebachew@users.noreply.github.com>
2024-05-12 21:09:34 -0500

    ass-1-7-vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,33 @@
-// import React, { useState } from 'react';
-// import TodoList1 from './TodoList1/TodoList1';
 
-// const App = () => {
-//   const [todoList, setTodoList] = useState([
-//     { id: 1, text: 'Example Todo 1' },
-//     { id: 2, text: 'Example Todo 2' },
-//     { id: 3, text: 'Example Todo 3' },
-//   ]);
-
-//   const removeTodo = (id) => {
-//     setTodoList(todoList.filter(todo => todo.id !== id));
-//   };
-
-//   return (
-//     <div>
-//       <h1>To-Do Application with Remove Buttons</h1>
-//       <TodoList1 todoList={todoList} onRemoveTodo={removeTodo} />
-//     </div>
-//   );
-// };
-
-// export default App;
-// Filter out the todo item with the given id
-// Update the state with the new todo list
-/* Pass removeTodo as a prop */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TodoList1 from './TodoList1/TodoList1';
 import AddTodoForm from './AddTodoForm/AddTodoForm';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
 
+  useEffect(() => {
+   
+    const fetchData = () => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve({ data: { todoList: todoList } });
+        }, 2000);
+      });
+    };
+
+    fetchData().then(result => {
+      
+      setTodoList(result.data.todoList);
+    });
+  }, []); 
+
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo]);
   };
 
   const removeTodo = (id) => {
-    
     const updatedTodoList = todoList.filter((todo, index) => index !== id);
-    
     setTodoList(updatedTodoList);
   };
 
